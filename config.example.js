0a9313041ee2a71089bd561c804a9db0c985a628
sam-nic <lesha.suschits@gmail.com>
2026-07-30 18:07:19 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,3 +1,2 @@
-// Скопируйте этот файл в config.js и вставьте ваш токен Planfix
-// config.js добавлен в .gitignore — не будет попадать в репозиторий
-window.PLANFIX_TOKEN = 'YOUR_TOKEN_HERE';
+window.PLANFIX_PROXY_URL =
+  "https://seppra-planfix-proxy.lesha-suschits.workers.dev";
